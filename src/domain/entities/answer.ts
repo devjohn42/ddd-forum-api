f38e7670ddaf235e677acd7e3a9f6d8940a705a3
devjohn42 <devjohncb@gmail.com>
2025-05-26 11:45:20 -0300

    :bricks: ci: maps relationships

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "node:crypto"
 
+interface AnswerProps {
+  content: string
+  authortId: string
+  questionId: string
+}
+
 export class Answer {
   public id: string
   public content: string
+  public authortId: string
+  public questionId: string
 
-  constructor(content: string, id?: string) {
-    this.content = content
+  constructor(props: AnswerProps, id?: string) {
+    this.content = props.content
+    this.authortId = props.authortId
+    this.questionId = props.questionId
     this.id = id ?? randomUUID()
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,21 @@
 import { randomUUID } from "node:crypto"
 
+interface QuestionProps {
+  title: string
+  content: string
+  authortId: string
+}
+
 export class Question {
   public id: string
   public title: string
   public content: string
+  public authortId: string
 
-  constructor(title: string, content: string, id?: string) {
-    this.title = title
-    this.content = content
+  constructor(props: QuestionProps, id?: string) {
+    this.title = props.title
+    this.content = props.content
+    this.authortId = props.authortId
     this.id = id ?? randomUUID()
   }
 }
