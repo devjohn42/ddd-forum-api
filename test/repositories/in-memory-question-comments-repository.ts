c5b112cbd96a090311c16633683038cfdd72a13b
devjohn42 <devjohncb@gmail.com>
2025-06-06 01:52:18 -0300

    :sparkles: feat: use case - creates the delete question comment

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -9,4 +9,24 @@ export class InMemoryQuestionCommentsRepository
   async create(questionComment: QuestionComment) {
     this.questionsComment.push(questionComment)
   }
+
+  async findById(id: string) {
+    const questionComment = this.questionsComment.find(
+      (q) => q.id.toString() === id,
+    )
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const questionCommentIndex = this.questionsComment.findIndex(
+      (q) => q.id === questionComment.id,
+    )
+
+    this.questionsComment.splice(questionCommentIndex, 1)
+  }
 }
