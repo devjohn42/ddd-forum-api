4ba6a6d155564b7e9061497c138fcb0be57c3c86
devjohn42 <devjohncb@gmail.com>
2025-06-05 21:13:02 -0300

    :sparkles: feat: entitie - creates the question-comment

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,38 +1,26 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
-import dayjs from 'dayjs'
-import { Slug } from './value-objects/slug'
 
-export interface QuestionProps {
+export interface QuestionCommentProps {
   authorId: UniqueEntityID
-  bestAnswerId?: UniqueEntityID
-  title: string
+  questionId: UniqueEntityID
   content: string
-  slug: Slug
   createdAt: Date
   updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends Entity<QuestionCommentProps> {
   get authorId() {
     return this.props.authorId
   }
 
-  get bestAnswerId() {
-    return this.props.bestAnswerId
-  }
-
-  get title() {
-    return this.props.title
+  get questionId() {
+    return this.props.questionId
   }
 
   get content() {
     return this.props.content
-  }
-
-  get slug() {
-    return this.props.slug
   }
 
   get createdAt() {
@@ -43,10 +31,6 @@ export class Question extends Entity<QuestionProps> {
     return this.props.updatedAt
   }
 
-  get isNew(): boolean {
-    return dayjs().diff(this.createdAt, 'days') <= 3
-  }
-
   get excerpt() {
     return this.content.substring(0, 120).trimEnd().concat('...')
   }
@@ -55,30 +39,18 @@ export class Question extends Entity<QuestionProps> {
     this.props.updatedAt = new Date()
   }
 
-  set title(title: string) {
-    this.props.title = title
-    this.props.slug = Slug.createFromText(title)
-    this.touch()
-  }
-
   set content(content: string) {
     this.props.content = content
     this.touch()
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-    this.props.bestAnswerId = bestAnswerId
-    this.touch()
-  }
-
   static create(
-    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    props: Optional<QuestionCommentProps, 'createdAt'>,
     id?: UniqueEntityID,
   ) {
     const question = new Question(
       {
         ...props,
-        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: props.createdAt ?? new Date(),
       },
       id,
