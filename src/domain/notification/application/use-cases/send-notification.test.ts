91cacea3c7852eca432000826151413a190bfc7d
devjohn42 <devjohncb@gmail.com>
2025-06-11 14:17:12 -0300

    :white_check_mark: test: creates a test to send a notification

---
+++ src/domain/notification/application/use-cases/send-notification.test.ts
@@ -0,0 +1,26 @@
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { SendNotificationUseCase } from './send-notification'
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: SendNotificationUseCase
+
+describe('Create Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sut = new SendNotificationUseCase(inMemoryNotificationsRepository)
+  })
+
+  it('should be able to send a notification', async () => {
+    const result = await sut.execute({
+      recipientId: '1',
+      title: 'Nova notificação',
+      content: 'Conteúdo da notificação',
+      attachmentsIds: ['1', '2'],
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.notifications[0]).toEqual(
+      result.value?.notification,
+    )
+  })
+})
