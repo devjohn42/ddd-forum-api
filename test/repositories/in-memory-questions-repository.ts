5167c63e1ae09b8fb7927d4c90cef96f4fa7f5c7
devjohn42 <devjohncb@gmail.com>
2025-06-11 20:03:16 -0300

    :sparkles: feat: creates the entire flow for when a best answer is chosen for a question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/share-repo/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -12,6 +13,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question) {
     this.questions.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async findBySlug(slug: string) {
@@ -46,6 +49,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const questionIndex = this.questions.findIndex((q) => q.id === question.id)
 
     this.questions[questionIndex] = question
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question) {
